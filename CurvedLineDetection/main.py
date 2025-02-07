87387828688b8e232612a35fb9e5044484013150
AldreiC <149686657+AldreiC@users.noreply.github.com>
2025-02-07 02:14:04 -0500

    Update main.py

--- CurvedLineDetection/main.py
+++ CurvedLineDetection/main.py
@@ -1,4 +1,19 @@
 # Aldrei Chua 1/23/2025
+"""
+Title: Image Thresholding
+Author: OpenCV
+Type: Source Code
+Availability: https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
+"""
+
+"""
+Title: Contour Features
+Author: OpenCV
+Type: Source Code
+Availability: https://docs.opencv.org/4.x/dd/d49/tutorial_py_contour_features.html
+"""
+
+
 import cv2
 import numpy as np
 
@@ -8,38 +23,63 @@ video = cv2.VideoCapture(0)
 
 
 def curve_detect(img):
-    gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    gray_img = cv2.blur(gray_img, (3, 3))
+    """Returns an image with an overlay of curved parallel lines and their centerline
 
-    _, mask = cv2.threshold(gray_img, 100, 150, cv2.THRESH_BINARY)
+    Detects curved parallel lines, calculates their centerline, and returns an image with the image overlay
+
+    Parameters
+    ------------
+        img: numpy.ndarray
+            An individual frame of the video obtained by the camera
+    Return
+    ------------
+        img: numpy.ndarray
+            The original image modified with image overlay detecting two curved parallel lines and their centerline
+    """
+
+    # Preprocess the raw image for detection
+    gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gray_img = cv2.blur(gray_img, (11, 11))
+
+    kernel = np.ones((7, 7), np.uint8)
+    gray_img = cv2.dilate(gray_img, kernel, iterations=1)
+
+    mask = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 11)
     gray_img = cv2.bitwise_and(gray_img, gray_img, mask=mask)
 
-    edges = cv2.Canny(gray_img, 100, 200, apertureSize=5)
+    edges = cv2.Canny(gray_img, 150, 200, apertureSize=5, L2gradient=True)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contour_list = []
 
-    lines = cv2.HoughLinesP(image=edges, rho=1, theta=np.pi/180, threshold=200, minLineLength=10, maxLineGap=10)
-    for line in lines:
-        x3, y3, x4, y4 = line[0]
-        cv2.line(img, (x3, y3), (x4, y4), (0, 255, 0), 3)
+    try:
+        # Detect all long, curved contours around the middle of the screen
+        for contour in contours:
+            epsilon = 0.001 * cv2.arcLength(contour, False)
+            approximate_contour = cv2.approxPolyDP(contour, epsilon, False)
 
-    """try:
-        x1, y1, x2, y2 = lines[0][0]
-        if x1 != x2:
-            m1 = (y2 - y1) / (x2 - x1)
-        else:
-            m1 = 10000
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+            length = cv2.arcLength(approximate_contour, False)
+            coordinate_total = approximate_contour[0][0][0] + approximate_contour[0][0][1]
+            if length <= 500 or coordinate_total <= (width+height) * 0.2 or coordinate_total >= (width+height) * 0.8:
+                continue
 
-        for line in lines:
-            x3, y3, x4, y4 = line[0]
-            if x3 != x4:
-                m2 = (y4 - y3) / (x4 - x3)
-            else:
-                m2 = 10000
+            contour_list.append(approximate_contour)
+            cv2.drawContours(img, [approximate_contour], -1, (0, 0, 255), 2)
 
-            if (int(m1 * m2) == -1) or (m1 == 0 and abs(m2) >= 1000) or (abs(m1) >= 1000 and m2 == 0):
-                cv2.line(img, (x3, y3), (x4, y4), (0, 255, 0), 3)
+        # Find and display the midline between two contours
+        c_line1 = contour_list[0]
+        c_line2 = contour_list[1]
+
+        midline = []
+        for point1, point2 in zip(c_line1, c_line2):
+            m_pointx = (point1[0][0] + point2[0][0]) // 2
+            m_pointy = (point1[0][1] + point2[0][1]) // 2
+            midline.append([m_pointx, m_pointy])
+
+        midline = np.array(midline, dtype=np.int32)
+        cv2.polylines(img, [midline], isClosed=False, color=(0, 255, 0), thickness=2)
+
     except:
-        pass"""
+        pass
 
     return img
 
@@ -48,10 +88,12 @@ while True:
     # Constantly reads the webcam feed and creates individual images for processing
 
     _, image = video.read()
+    height, width = image.shape[:2]
     imgResult = curve_detect(image)
 
-    cv2.imshow("Curved Line Detection", imgResult)  # Displays a window with the image overlay
+    # Display a window with the image overlay
+    cv2.imshow("Curved Line Detection", imgResult)
 
-    # Closes the window when the space bar is pressed
+    # Close the window when the space bar is pressed
     if cv2.waitKey(1) == ord(' '):
         break
